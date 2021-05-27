c562e7e77097d59ff85d62de437e03b17576c80b
Benny <bbxblack@gmail.com>
2021-05-27 11:29:11 +0200

    esercizio live

--- MyLavarelLive/app/Models/Cars.php
+++ MyLavarelLive/app/Models/Cars.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cars extends Model
 {
-    //
+    protected $fillable = [
+        'brand',
+        'model',
+        'displacement',
+        'max_speed',
+    ];
 }
